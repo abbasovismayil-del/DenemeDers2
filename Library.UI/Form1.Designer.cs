c9142000917f8a207bc970e6656510828ce3233d
Ugur Sahin <ugursahin423@gmail.com>
2025-11-02 21:53:55 +0300

    Proje tamamlandı

--- Library.UI/Form1.Designer.cs
+++ Library.UI/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            lstBooks = new ListBox();
+            lstAuthors = new ListBox();
+            lstMembers = new ListBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 381);
+            button1.Name = "button1";
+            button1.Size = new Size(318, 57);
+            button1.TabIndex = 0;
+            button1.Text = "Ekle";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // lstBooks
+            // 
+            lstBooks.FormattingEnabled = true;
+            lstBooks.Location = new Point(2, 12);
+            lstBooks.Name = "lstBooks";
+            lstBooks.Size = new Size(786, 104);
+            lstBooks.TabIndex = 1;
+            // 
+            // lstAuthors
+            // 
+            lstAuthors.FormattingEnabled = true;
+            lstAuthors.Location = new Point(2, 122);
+            lstAuthors.Name = "lstAuthors";
+            lstAuthors.Size = new Size(786, 104);
+            lstAuthors.TabIndex = 2;
+            // 
+            // lstMembers
+            // 
+            lstMembers.FormattingEnabled = true;
+            lstMembers.Location = new Point(2, 232);
+            lstMembers.Name = "lstMembers";
+            lstMembers.Size = new Size(786, 104);
+            lstMembers.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(352, 381);
+            button2.Name = "button2";
+            button2.Size = new Size(238, 57);
+            button2.TabIndex = 4;
+            button2.Text = "Sil";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(lstMembers);
+            Controls.Add(lstAuthors);
+            Controls.Add(lstBooks);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private ListBox lstBooks;
+        private ListBox lstAuthors;
+        private ListBox lstMembers;
+        private Button button2;
     }
 }
--- Library.UI/Form1.cs
+++ Library.UI/Form1.cs
@@ -1,10 +1,110 @@
+﻿using Library.Business.Services;
+using Library.DataAcces.Abstractions;
+using Library.DataAcces.Context;
+using Library.Entity.Models;
+
 namespace Library.UI
 {
     public partial class Form1 : Form
     {
+        private readonly BookService _bookService;
+        private readonly AuthorService _authorService;
+        private readonly MembersService _membersService;
+        private readonly IUnitOfWork _unitOfWork;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            _unitOfWork = new UnitOfWork(new ApplicationDBContext());
+            _bookService = new BookService(_unitOfWork);
+            _authorService = new AuthorService(_unitOfWork);
+            _membersService = new MembersService(_unitOfWork);
+
+            GetAllData();
+
+        }
+
+        private void GetAllData()
+        {
+            lstAuthors.DataSource = null;
+            lstAuthors.DisplayMember = "AuthorName";
+            lstAuthors.ValueMember = "ID";
+            lstAuthors.DataSource = _authorService.GetAll();
+
+            lstBooks.DataSource = null;
+            lstBooks.DisplayMember = "BookName";
+            lstBooks.ValueMember = "ID";
+            lstBooks.DataSource = _bookService.GetAll();
+
+            lstMembers.DataSource = null;
+            lstMembers.DisplayMember = "NameSurname";
+            lstMembers.ValueMember = "ID";
+            lstMembers.DataSource = _membersService.GetAll();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Author a = new Author();
+            a.AuthorName = "Dostoyevski";
+            a.Nationality = "Rus";
+            _authorService.Add(a);
+            _unitOfWork.complete();
+
+            Books S = new Books();
+            S.BookName = "Suc ve Ceza";
+            S.Publisher = "Is bankası yayınları";
+            S.Author = a;
+            _bookService.Add(S);
+            _unitOfWork.complete();
+
+            Member m = new Member();
+            m.NameSurname = "Ugur Sahin";
+            m.PhoneNumber = "05516295292";
+            m.Address = "Ankara";
+            _membersService.Add(m);
+            _unitOfWork.complete();
+
+            GetAllData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAllData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lstAuthors.SelectedIndex != -1 && lstBooks.SelectedIndex != -1 && lstMembers.SelectedIndex != -1)
+                {
+                    var authorID = (Guid)lstAuthors.SelectedValue;
+
+                    _authorService.Delete(authorID);
+                    _unitOfWork.complete();
+
+                    var bookID = (Guid)lstBooks.SelectedValue;
+
+                    _bookService.Delete(bookID);
+                    _unitOfWork.complete();
+
+                    var memberID = (Guid)lstMembers.SelectedValue;
+
+                    _membersService.Delete(memberID);
+                    _unitOfWork.complete();
+
+                    GetAllData();
+                    MessageBox.Show("Silme işlemi başarılı.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
